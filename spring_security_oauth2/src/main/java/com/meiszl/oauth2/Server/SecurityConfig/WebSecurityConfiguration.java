8b96776dc885771bc4e038f0d85343d262059261
meiszl <tiaode@qq.com>
2019-05-05 20:20:50 +0800

    2019-05-05 20:20

---
+++ spring_security_oauth2/src/main/java/com/meiszl/oauth2/Server/SecurityConfig/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.meiszl.oauth2.Server.SecurityConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 基于内存存储用户名密码
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("123456")).roles("USERS");
+    }
+}
---
+++ spring_security_oauth2/src/main/java/com/meiszl/oauth2/oauth2Application.java
@@ -0,0 +1,12 @@
+package com.meiszl.oauth2;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oauth2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(oauth2Application.class,args);
+    }
+}
